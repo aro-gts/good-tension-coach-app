34e16c298325cbbfa1f7a4d46bca04c3c430abe5
Araceli Ruiz O'Clair <araceli@goodtension.net>
2025-07-20 21:09:56 -0600

    Create supabase.js

---
+++ public/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://zmehmjwlzahsvvrmtqel.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptZWhtandsemFoc3V2cm10cWVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MjA0NDUsImV4cCI6MjA2NjM5NjQ0NX0.BDvCG-WLrdJ6ZkTzG2TSrXJwaFz2Kom7jmt3o217ixE';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
